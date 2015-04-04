c8ab459179feb09b986e49e5a34e364c64094034
Stefan Parzentny <s.parzentny@x-ion.de>
2015-04-04 03:55:34 +0200

    added belongs_to relations for acrobatic_ratings and dance_team

--- app/models/acrobatic_rating.rb
+++ app/models/acrobatic_rating.rb
@@ -1,3 +1,4 @@
 class AcrobaticRating < ActiveRecord::Base
   belongs_to :acrobatic
+  belongs_to :dance_team
 end
--- app/models/dance_team.rb
+++ app/models/dance_team.rb
@@ -1,5 +1,6 @@
 class DanceTeam < ActiveRecord::Base
   belongs_to :club
+  belongs_to :dance_class
   has_and_belongs_to_many :dancers
   has_many :dance_ratings
   has_many :acrobatic_ratings
