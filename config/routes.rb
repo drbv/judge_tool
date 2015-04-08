de0f6c87e9046dea8938899192f02d849f03bfa0
Bente Pieck <b.pieck@x-ion.de>
2015-04-09 00:01:35 +0200

    added some routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    resources :users, :rounds
+  end
+  namespace :judges do
+    resource :dance_round
+  end
+  resource :round, only: %i(show)
   resources :pages, only: :show
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
   root to: 'rounds#index'
 end
