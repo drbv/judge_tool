5e38e036cbdd89085a3f6f4b4859cdd0cbd27477
Bente Pieck <b.pieck@x-ion.de>
2015-05-10 22:03:55 +0200

    refactored and dried out more of judge status stuff - and added color for judge with reopened discussion

--- app/helpers/judges/dance_rounds_helper.rb
+++ app/helpers/judges/dance_rounds_helper.rb
@@ -1,2 +1,13 @@
 module Judges::DanceRoundsHelper
+  def judge_status_class(judge)
+    if judge.rated?(current_dance_round)
+      if judge.open_discussions?(current_dance_round)
+        'alert-info'
+      else
+        'alert-success'
+      end
+    else
+      'alert-danger'
+    end
+  end
 end
