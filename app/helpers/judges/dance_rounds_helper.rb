d436f962ae93ff81d1ccb2f783862960689fcf28
Bente Pieck <b.pieck@x-ion.de>
2015-05-11 03:44:44 +0200

    ensure, that judge already rated

--- app/helpers/judges/dance_rounds_helper.rb
+++ app/helpers/judges/dance_rounds_helper.rb
@@ -15,7 +15,7 @@ module Judges::DanceRoundsHelper
     html_classes = []
     if attr != :full_mistakes
       html_classes << 'markable' if current_dance_round.ready?(current_user)
-      html_classes << 'danger' if rating.diff_to_big?(attr)
+      html_classes << 'danger' if rating && rating.diff_to_big?(attr)
     end
     html_classes.empty? && html_classes.join(' ')
   end
