76ca65b99a2fc7b7cd109eb6c44270afdad31f2b
Stefan Parzentny <s.parzentny@x-ion.de>
2016-06-06 01:25:04 +0200

    layout bugfix

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
-set :branch, ask('branch to deploy ( default master )', 'master', echo: true)
+set :branch, ask('branch to deploy', 'master', echo: true)
 set :server_ip, ask('Give the server ip', nil, echo: true)
-set :user, ask('user ( default judge )', 'judge', echo: true)
+set :user, ask('user', 'judge', echo: true)
 server fetch(:server_ip),user: fetch(:user), roles: %{web app db}
