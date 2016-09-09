ce8079fe0c8bf093fcdcd015f00f5eb24ccd9e07
Stefan Parzentny <s.parzentny@x-ion.de>
2016-09-09 18:46:02 +0200

    fixed missing i

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
 set :branch, ask('branch to deploy', 'master', echo: true)
 set :server_ip, ask('Give the server ip', nil, echo: true)
 set :user, ask('user', 'judge', echo: true)
-server fetch(:server_ip),user: fetch(:user), roles: %{web app db}
+server fetch(:server_ip),user: fetch(:user), roles: %i{web app db}
