56463b3db79b5746a61765f2f27824ed7b3996ec
Stefan Parzentny <s.parzentny@x-ion.de>
2017-01-30 03:27:30 +0100

    initialising ews1 data on startup

---
+++ config/initializers/ews1_data_import.rb
@@ -0,0 +1,5 @@
+ews1_data = YAML.load_file "config/ews1.conf"
+$ews1_ip = ews1_data["ews1_ip"]
+$ews1_password = ews1_data["ews1_password"]
+$ews1_tournamentnr = ews1_data["ews1_tournamentnr"]
+$ews1_use_auto_upload = ews1_data["ews1_use_auto_upload"]
