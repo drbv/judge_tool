5426c6f8512d99973d4c0e7fdf57a181e54975c4
Stefan Parzentny <s.parzentny@x-ion.de>
2017-01-30 03:28:04 +0100

    added config methods for ews1 data inclusive feedback

--- app/controllers/admin/utilities_controller.rb
+++ app/controllers/admin/utilities_controller.rb
@@ -1,5 +1,6 @@
 class Admin::UtilitiesController < Admin::BaseController
   require 'uri'
+  require 'net/http'
   def index
   end
 
@@ -19,14 +20,77 @@ class Admin::UtilitiesController < Admin::BaseController
     redirect_to admin_utilities_index_path
   end
 
+  def update_ews1_config_file
+    ews1_conf_file = "config/ews1.conf"
+    ews1_data = YAML.load_file ews1_conf_file
+    ews1_data["ews1_ip"] = $ews1_ip
+    ews1_data["ews1_password"] = $ews1_password
+    ews1_data["ews1_tournamentnr"] = $ews1_tournamentnr
+    ews1_data["ews1_use_auto_upload"] = $ews1_use_auto_upload
+    File.open(ews1_conf_file, 'w') { |f| YAML.dump(ews1_data, f) }
+
+  end
+
+  def set_ews1_endpoint
+    $ews1_ip = params[:ip]
+    $ews1_password = params[:password]
+    # check if there is an Tournament already
+    $ews1_tournamentnr = params[:tournamentnr]
+
+    if !params[:use_auto_upload].nil?
+      $ews1_use_auto_upload = true
+    else
+      $ews1_use_auto_upload = false
+    end
+    update_ews1_config_file
+    check_ews1_endpoint
+  end
+
+  def reset_ews1_config_file
+    $ews1_ip = "0.0.0.0"
+    $ews1_password = "supersecret"
+    # check if there is an Tournament already
+    $ews1_tournamentnr = "1234567"
+    $ews1_use_auto_upload = true
+    update_ews1_config_file
+    return true
+  end
+
+  def check_ews1_endpoint
+    uri = URI("http://#{$ews1_ip}:8080/T#{$ews1_tournamentnr}_TDaten.mdb")
+    req = Net::HTTP::Get.new(uri)
+    req.basic_auth 'ews2', $ews1_password
+    begin
+    res = Net::HTTP.start(uri.hostname, uri.port) {|http|
+      http.request(req)
+    }
+    if res.response.kind_of? Net::HTTPSuccess
+      flash[:success]=" Verbindugn zum ews1 vorhanden"
+    elsif res.response.kind_of? Net::HTTPUnauthorized
+      flash[:danger]=" Passwort nicht gültig"
+    elsif res.response.kind_of? Net::HTTPNotFound
+      flash[:warning]=" Turnierdatei konnte nicht gefunden werden"
+    elsif res.response.kind_of? Net::HTTPRequestTimeOut
+      flash[:danger]=" Verbindungs Timeout"
+    else
+      flash[:danger]=" Verbindung nicht möglich. Einstellungen / Verbindung prüfen"
+    end
+    rescue
+      flash[:danger]=" Verbindung nicht möglich"
+    end
+    redirect_to admin_utilities_index_path
+  end
+
   def judge_tool_reset
-    delete_mdb=value=system('rm tmp/*.mdb')
+    delete_mdb=system('rm tmp/*.mdb')
     drop_table=system("RAILS_ENV=#{Rails.env} rake db:drop")
     migrate_table=system("RAILS_ENV=#{Rails.env} rake db:migrate")
+    reset_ews1_config_file
     flash[:success]="
 </br> Access Datenbank gelöscht/unlinked: #{delete_mdb}
 </br> JudgeTool Datenbank gelöscht: #{drop_table}
 </br> JudgeTool Datenbank erstellt: #{migrate_table}
+</br> ews1 Daten zurückgesetzt: #{reset_ews1_config_file}
 </br> Passwort zurückgesetzt! user:admin pin:1234
 </br> <h1>Prozess oder Server neustarten</h1>"
 
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
 
   get 'admin/utilities/index'
   post 'admin/utilities/db_upload'
+  post 'admin/utilities/set_ews1_endpoint'
+  get 'admin/utilities/check_ews1_endpoint'
   get 'admin/utilities/judge_tool_reset'
 
 
