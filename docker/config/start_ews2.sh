e4e584d764737693f26ce9c3263c54d07023b8ba
Stefan Parzentny <s.parzentny@x-ion.de>
2017-04-23 21:10:00 +0200

    modified startup script to make restart stable

--- docker/config/start_ews2.sh
+++ docker/config/start_ews2.sh
@@ -4,4 +4,5 @@ cd /root
 ruby bin/websocket_for_observer.rb
 ruby bin/websocket_for_judge_status.rb
 ruby bin/ws_for_beamer.rb
-bundle exec unicorn -c /root/config/unicorn.rb -E production 
+rm tmp/pids/unicorn.pid
+unicorn -c /root/config/unicorn.rb -E production 
